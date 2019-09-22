e258d7f21a5e60e6e699bea7bab824b5357bbd2a
birna <briandam26@yahoo.com>
2019-09-22 16:59:28 -0700

    Implemented a a game over and ability to hit ball

--- app.js
+++ app.js
@@ -78,25 +78,28 @@ function draw() {
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    if(y + dy < ballRadius) {
         dy = -dy;
     }
-
-    if(rightPressed) {
+    else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval); // Needed for Chrome to end game
+        }
+    }
+    
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
         paddleX += 7;
-        if (paddleX + paddleWidth > canvas.width){
-            paddleX = canvas.width - paddleWidth;
-        }
     }
-    else if(leftPressed) {
+    else if(leftPressed && paddleX > 0) {
         paddleX -= 7;
-        if (paddleX < 0){
-            paddleX = 0;
-        }
     }
-
     x += dx;
     y += dy;
 }
 
-setInterval(draw, 10);
+var interval = setInterval(draw, 10);
